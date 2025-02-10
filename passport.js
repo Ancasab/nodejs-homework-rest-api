86bc9b7d95683c17668f6da351b090bbc1781c32
Anca_Sab <asabadeanu@gmail.com>
2025-02-10 10:53:44 +0200

    Cleanup - Paasport.js

--- passport.js
+++ passport.js
@@ -30,37 +30,3 @@ passport.use(
 export default passport;
 
 
-// import passport from 'passport';
-// import passportJWT from 'passport-jwt';
-// import dotenv from 'dotenv';
-// import User from './models/users.js';
-
-// dotenv.config();
-
-
-// const secret = process.env.TOKEN_SECRET;
-
-// const ExtractJWT = passportJWT.ExtractJwt;
-// const Strategy = passportJWT.Strategy;
-
-// const params = {
-//   secretOrKey: secret,
-//   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-// };
-
-// passport.use(
-//   new Strategy(params, async function (payload, done) {
-//     try {
-//       const user = await User.findOne({ email: payload.data.email });  
-//       if (!user) {
-//         return done(null, false); 
-//       }
-//       return done(null, user);
-      
-//     } catch (err) {
-//       return done(err, false);
-//     }
-//   })
-// );
-
-// export default passport;
